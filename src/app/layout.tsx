2909aa8dd90e949cd7b38557e73ad1a46469605a
renewablezip <renewablezip01@gmail.com>
2024-05-09 02:18:04 +0200

    Move styles into src/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../../styles/globals.scss";
-import "../../styles/tailwind.css";
+import "@/styles/globals.scss";
+import "@/styles/tailwind.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
