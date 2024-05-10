9d90e80dde9ee6a819ccebca44d1340e73c4e08d
renewablezip <renewablezip01@gmail.com>
2024-05-10 23:07:48 +0200

    Update Dropdown component

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -5,11 +5,9 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 export default function Dropdown({ options }: { options: string[] }) {
   return <>
     <Menu>
-      <MenuButton>
-        <button type={"button"} className="flex text-white items-center py-1 px-4 bg-white bg-opacity-10 text-s border-none gap-2 !rounded-sm">
-          <span>Sort</span>
-          <FontAwesomeIcon icon={faChevronDown} aria-haspopup="true" aria-controls="dropdown-menu5" width={15} />
-        </button>
+      <MenuButton type="button" className="flex text-white items-center py-1 px-4 bg-white bg-opacity-10 text-s border-none gap-2 !rounded-sm">
+        <span>Sort</span>
+        <FontAwesomeIcon icon={faChevronDown} aria-haspopup="true" aria-controls="dropdown-menu5" width={15} />
       </MenuButton>
       <MenuItems anchor="bottom" className={"flex flex-col bg-zinc-800 mt-2 rounded-sm  gap-2 data-[focus]:bg-blue-100"}>
         {
