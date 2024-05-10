28f9207a40b6a22995a341cffbf36bd89d009764
renewablezip <renewablezip01@gmail.com>
2024-05-10 23:06:08 +0200

    Move page body to components for reuse

---
+++ src/components/body.tsx
@@ -0,0 +1,17 @@
+import { ReactNode, Children } from "react";
+
+export default function BodyWrapper({ children, className }: { children: ReactNode, className?: string }) {
+  return <>
+    <div className={`flex flex-col items-center mx-4 ${className}`}>
+      <div className="flex flex-col items-center w-full max-w-[1100px] h-full py-4 gap-5">
+        {
+          Children.map(children, (child, index) => (
+            <div key={index} className="flex flex-col gap-3 bg-primary justify-center h-full py-8 px-10 rounded-sm  w-full">
+              {child}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  </>;
+}
