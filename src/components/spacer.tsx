ab7b69ab040924909e1fff2e99de6d2dcb4fbec6
renewablezip <renewablezip01@gmail.com>
2024-05-10 23:06:56 +0200

    Update Spacer component

--- src/components/spacer.tsx
+++ src/components/spacer.tsx
@@ -1,3 +1,3 @@
-export default function Spacer({ className }: { className: string | null }) {
+export default function Spacer({ className }: { className?: string }) {
   return <div className={`bg-white w-full h-[1px] bg-opacity-15 ${className}`} />
 }
